127293fcd592129f5ca12eba3851097c2145d763
Your Name <you@example.com>
2019-12-02 14:18:14 +0000

    before gaurav

---
+++ git_hook.py
@@ -0,0 +1,28 @@
+from flask import Flask, request
+import json
+import hmac
+import os
+import hashlib
+
+app = Flask(__name__)
+
+SECRET_KEY = os.getenv('WEBHOOK_SECRET_KEY')
+
+def validate_signature(data):
+    sha_name, signature = request.headers['X-Hub-Signature'].split('=')
+    if sha_name != 'sha1':
+        return False
+
+    mac = hmac.new(str(SECRET_KEY), msg=str(data), digestmod=hashlib.sha1)
+    return hmac.compare_digest(mac.hexdigest(), str(signature))
+
+@app.route('/payload',methods=['POST'])
+def foo():
+   data = json.loads(request.data)
+   #print (data)
+   flag = validate_signature(request.data)
+   #print(flag)
+   return "OK"
+
+if __name__ == '__main__':
+   app.run(host='0.0.0.0', port=8000)
